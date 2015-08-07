cdd92c87687fa0b19b0da20e9689aaf241b28673
Gardezi1 <ali.gardezi@kwanso.com>
2015-08-07 19:29:27 +0500

    Project Version 1

--- client/templates/news/news_submission.js
+++ client/templates/news/news_submission.js
@@ -1,14 +1,15 @@
 Template.newsSubmisson.events({
   "submit form": function (event, template) {
-    var tit = event.target.inputtitle.value;
+    var titl = event.target.inputtitle.value;
     var u = event.target.hackerNews_title.value;
     var des = event.target.hackerNews_description.value
 
+    if(titl != null && u != null && des != null){
     Posts.insert({
       title: tit,
       url: u,
       description: des
-    });
+    });}
 
   }
 });
